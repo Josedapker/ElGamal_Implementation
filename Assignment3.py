7056b0d51b9b7ce674e0e4b1a298a3641c7562da
Josedapker <91170378+Josedapker@users.noreply.github.com>
2023-03-09 14:28:44 -0500

    Create Assignment3.py
    Starting Assignment 3

---
+++ Assignment3.py
@@ -0,0 +1,34 @@
+import math
+
+a = ""      # 1 < a < p-1
+p = ""
+g = ""
+e = g ** a % p
+
+def generate_keys(p,g,e):
+    #e = g ** a % p
+    public_key = ''
+    return public_key(p,g,e)
+
+
+
+
+
+def encryption():
+    #C_1 = g**b % p
+    #C_2 = m * e**b % p
+    #Cipher text = (C_1, C_2)
+    return
+
+
+def decryption():
+    
+    return
+
+def main():
+    generate_keys(p,g,e)
+    encryption()
+
+    #decryption
+
+    return
